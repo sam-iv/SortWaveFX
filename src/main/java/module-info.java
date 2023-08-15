cc11eb429cf8a504a525e2b7e53d35021c5a8c86
Samuel Ivuerah <samivuerah@gmail.com>
2023-08-15 09:46:43 +0100

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.sortwavefx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires com.almasb.fxgl.all;
+
+    opens com.sortwavefx to javafx.fxml;
+    exports com.sortwavefx;
+}
